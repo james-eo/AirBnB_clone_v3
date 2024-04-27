fdf719621066e40d808652994c42930dcf0a17f7
ahmed <ahmedissaten@gmail.com>
2024-04-27 16:50:56 +0200

    update app.py: handle 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Start API
 app.py
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 
 from api.v1.views import app_views
@@ -23,6 +23,22 @@ def teardown(exception):
     teardown function
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return (resp)
 
 
 if __name__ == "__main__":
