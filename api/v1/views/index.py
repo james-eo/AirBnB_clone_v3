6001e2c8bbc1dada3ed3f82656f33983794f1ddc
james-eo <jameseokonkwo@gmail.com>
2024-04-28 16:21:50 +0100

    Create State, City, Places, Amenities

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_status():
-    """Return a JSON response with the status."""
-    return jsonify({"status": "OK"})
+    """Retrieves the number of each object type"""
+    counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(counts)
