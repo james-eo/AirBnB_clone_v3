869d2ac5441b47552c0e78a82d7b3b7edc5b2369
james-eo <jameseokonkwo@gmail.com>
2024-04-28 18:18:55 +0100

    api status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
+"""Create an endpoint that retrieves the
+number of each objects by type
+"""
 
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_status():
-    """Retrieves the number of each object type"""
-    counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(counts)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_api_status():
+    """Gets the status of the API"""
+    return jsonify({"status": "OK"})
