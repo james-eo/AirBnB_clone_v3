4b958990231dc7b201bc811c7d1055b00c759e3e
james-eo <jameseokonkwo@gmail.com>
2024-04-30 14:08:04 +0100

    Hash password using MD5

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,8 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-from hashlib import md5
+import hashlib
+from models import storage
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -59,18 +60,16 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, show_password=False, include_datetime_format=True):
-        """Returns a dictionary representation of the model instance."""
-        my_dict = dict(**self.__dict__)
-
-        if not show_password:
-            my_dict.pop('password', None)
-        if include_datetime_format:
-            for key, value in my_dict.items():
-                if isinstance(value, datetime.datetime):
-                    my_dict[key] = value.strftime("%Y-%m-%d %H:%M:%S")
-        my_dict.pop('_sa_instance_state', None)
-        return my_dict
+    def to_dict(self, include_password=False):
+        """Returns a dictionary representation of the object"""
+        dict_repr = self.__dict__.copy()
+        if not include_password:
+            dict_repr.pop('password', None)
+        dict_repr['created_at'] = self.created_at.isoformat()
+        dict_repr['updated_at'] = self.updated_at.isoformat()
+        dict_repr['__class__'] = self.__class__.__name__
+        dict_repr.pop('_sa_instance_state', None)
+        return dict_repr
 
     '''
     def to_dict(self):
